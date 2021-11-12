bacb59c268e99cabff9e8657c57aa0dab85ad907
Alex Anghel <alexisky@gmail.com>
2021-11-12 13:34:52 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./Home";
+
+
+const App = () => (
+    <>
+        <h1>Welcome to React Vite Micro App!</h1>
+        <p>Hard to get more minimal than this React app.</p>
+
+        <Home/>
+    </>
+);
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+
+const Home = () => {
+    const [clients, setClients] = useState();
+
+    return (
+        <>
+            <h1>Home</h1>
+
+        </>
+    )
+};
+
+export default Home;
